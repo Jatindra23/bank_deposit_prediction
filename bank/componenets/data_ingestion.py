370e865917ec0a134f5823814ba9170cb3eb8aa6
jatin9091@gmail.com <jatin9091@gmail.com>
2024-08-14 02:02:00 +0530

    data validation changes

--- bank/componenets/data_ingestion.py
+++ bank/componenets/data_ingestion.py
@@ -12,14 +12,13 @@ from bank.constant.database import COLLECTION_NAME
 from sklearn.model_selection import train_test_split
 
 
-
 class DataIngestion:
-    def __init__(self,data_ingestion_config:DataIngestionConfig):
+    def __init__(self, data_ingestion_config: DataIngestionConfig):
         try:
-            self.data_ingestion_config=data_ingestion_config
-            
+            self.data_ingestion_config = data_ingestion_config
+
         except Exception as e:
-            raise BankException(e,sys)
+            raise BankException(e, sys)
 
     def export_data_into_feature_store(self) -> DataFrame:
         """
@@ -30,28 +29,29 @@ class DataIngestion:
 
             bank_data = BankData()
 
-            dataframe = bank_data.export_collection_as_dataframe(collection_name=self.data_ingestion_config.collection_name)
-            
-            feature_store_file_path = self.data_ingestion_config.feature_store_file_path            
+            dataframe = bank_data.export_collection_as_dataframe(
+                collection_name=self.data_ingestion_config.collection_name
+            )
 
-            #creating folder
+            feature_store_file_path = self.data_ingestion_config.feature_store_file_path
+
+            # creating folder
 
             dir_path = os.path.dirname(feature_store_file_path)
-            os.makedirs(dir_path,exist_ok=True)
-            
-            dataframe.to_csv(feature_store_file_path,index=False,header=True)
+            os.makedirs(dir_path, exist_ok=True)
+
+            dataframe.to_csv(feature_store_file_path, index=False, header=True)
             return dataframe
-        
-        except  Exception as e:
-            raise  BankException(e,sys)
-        
 
-
+        except Exception as e:
+            raise BankException(e, sys)
 
     def split_data_as_train_test(self, dataframe: DataFrame) -> None:
         try:
             train_set, test_set = train_test_split(
-                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
+                dataframe,
+                test_size=self.data_ingestion_config.train_test_split_ratio,
+                random_state=42,
             )
 
             logging.info("Performed train test split on the dataframe")
@@ -76,23 +76,25 @@ class DataIngestion:
 
             logging.info(f"Exported train and test file path.")
         except Exception as e:
-            raise BankException(e,sys)
-     
+            raise BankException(e, sys)
 
     def initiate_data_ingestion(self) -> DataIngestionArtifact:
         try:
             bank_data = BankData()
-            bank_data.save_csv_file(file_path=FILE_NAME,collection_name= COLLECTION_NAME)
+            bank_data.save_csv_file(
+                file_path=FILE_NAME, collection_name=COLLECTION_NAME
+            )
 
             dataframe = self.export_data_into_feature_store()
 
-        
             self.split_data_as_train_test(dataframe=dataframe)
 
-            data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
-            test_file_path=self.data_ingestion_config.testing_file_path)
+            data_ingestion_artifact = DataIngestionArtifact(
+                trained_file_path=self.data_ingestion_config.training_file_path,
+                test_file_path=self.data_ingestion_config.testing_file_path,
+            )
 
             return data_ingestion_artifact
-        
+
         except Exception as e:
-            raise BankException(e,sys)
+            raise BankException(e, sys)
--- bank/componenets/data_validation.py
+++ bank/componenets/data_validation.py
@@ -5,7 +5,7 @@ from bank.entity.config_entity import DataValidationConfig
 from bank.exception import BankException
 from bank.logger import logging
 from bank.utils.main_utils import read_yaml_file, write_yaml_file
-from scipy.stats import ks_2samp
+from scipy.stats import ks_2samp, chi2_contingency
 import pandas as pd
 import os
 import sys
@@ -39,9 +39,8 @@ class DataValidation:
 
         except Exception as e:
             raise BankException(e, sys)
-        
 
-    def is_numerical_column_exist(self,dataframe:pd.DataFrame)-> bool:
+    def is_numerical_column_exist(self, dataframe: pd.DataFrame) -> bool:
         try:
 
             numerical_columns = self._schema_config["numerical_columns"]
@@ -52,7 +51,7 @@ class DataValidation:
 
             for num_column in numerical_columns:
                 if num_column not in dataframe_columns:
-                    numerical_column_poresent = False
+                    numerical_column_present = False
                     missing_numericla_columns.append(num_column)
 
             logging.info(f"Missing numerical columns: {missing_numericla_columns}")
@@ -60,10 +59,31 @@ class DataValidation:
             return numerical_column_present
 
         except Exception as e:
-            raise BankException(e,sys)
-        
+            raise BankException(e, sys)
+
+    def is_categorical_column_exist(self, dataframe: pd.DataFrame) -> bool:
+        try:
+
+            categorical_columns = self._schema_config["categorical_columns"]
+            dataframe_columns = dataframe.columns
+
+            categorical_column_present = True
+            missing_categorical_columns = []
+
+            for cat_column in self._schema_config["categorical_columns"]:
+                if cat_column not in dataframe.columns:
+                    categorical_column_present = False
+                    missing_categorical_columns.append(cat_column)
+
+            logging.info(f"Missing categorical columns: {missing_categorical_columns}")
+
+            return categorical_column_present
+
+        except Exception as e:
+            raise BankException(e, sys)
+
     @staticmethod
-    def read_data(file_path)->pd.DataFrame:
+    def read_data(file_path) -> pd.DataFrame:
         """
         Reads a CSV file from the given file path and returns the data as a pandas DataFrame.
 
@@ -77,15 +97,15 @@ class DataValidation:
             BankException: If an error occurs while reading the CSV file.
         """
         try:
-            
+
             return pd.read_csv(file_path)
 
         except Exception as e:
-            raise BankException (e,sys)
-        
+            raise BankException(e, sys)
 
-
-    def detect_dataset_drift(self, base_df, current_df, threshold: float = 0.05) -> bool:
+    def detect_dataset_drift(
+        self, base_df, current_df, threshold: float = 0.05
+    ) -> bool:
         try:
             status = True
             report = {}
@@ -94,76 +114,98 @@ class DataValidation:
                 d1 = base_df[column]
                 d2 = current_df[column]
 
-                is_same_dist = ks_2samp(d1,d2)
-                if threshold<=is_same_dist.pvalue:
+                is_same_dist = ks_2samp(d1, d2)
+                if threshold <= is_same_dist.pvalue:
                     is_found = False
 
                 else:
                     is_found = True
                     status = False
-                
-                report.update({column:{
-                    "p_value":float(is_same_dist.pvalue),
-                    "drift_status":is_found
-                }})
+
+                report.update(
+                    {
+                        column: {
+                            "p_value": float(is_same_dist.pvalue),
+                            "drift_status": is_found,
+                        }
+                    }
+                )
+
+            for column in base_df.select_dtypes(include=["object"]).columns:
+                d1 = base_df[column]
+                d2 = current_df[column]
+
+                # Create contingency table
+                contingency_table = pd.crosstab(d1, d2)
+                chi2, p_value, dof, ex = chi2_contingency(
+                    contingency_table, correction=False
+                )
+
+                if p_value < threshold:
+                    is_found = False
+
+                else:
+                    is_found = True
+                    status = False
+
+                report.update(
+                    {column: {"p_value": float(p_value), "drift_status": not is_found}}
+                )
 
             drift_report_file_path = self.data_validation_config.drift_report_file_path
 
-            #create directory
+            # create directory
             dir_path = os.path.dirname(drift_report_file_path)
             os.makedirs(dir_path, exist_ok=True)
 
-            write_yaml_file(file_path = drift_report_file_path, content = report)
+            write_yaml_file(file_path=drift_report_file_path, content=report)
 
             return status
 
         except Exception as e:
-            raise BankException (e,sys)
-
-        except Exception as e:
-            raise BankException (e,sys)
-        
+            raise BankException(e, sys)
 
     def initiate_data_validation(self) -> DataValidationConfig:
 
         try:
             error_message = ""
             train_file_path = self.data_ingestion_artifact.trained_file_path
-            test_file_path  = self.data_ingestion_artifact.test_file_path
+            test_file_path = self.data_ingestion_artifact.test_file_path
 
-            #reading data from train and test file
+            # reading data from train and test file
             train_dataframe = DataValidation.read_data(train_file_path)
             test_dataframe = DataValidation.read_data(test_file_path)
 
-            #validate number of columns
-            status = self.validate_number_of_columns(dataframe = train_dataframe)
+            # validate number of columns
+            status = self.validate_number_of_columns(dataframe=train_dataframe)
             if not status:
                 error_message = f"{error_message} Train dataframe doesn't contain all the columns.\n"
-            
-            status = self.validate_number_of_columns(dataframe = test_dataframe)
+
+            status = self.validate_number_of_columns(dataframe=test_dataframe)
             if not status:
-                error_message = f"{error_message} test dataframe doen't contain all the columns.\n"
+                error_message = (
+                    f"{error_message} test dataframe doen't contain all the columns.\n"
+                )
 
             if len(error_message) > 0:
-                raise Exception (error_message)
-            
-            status = self.detect_dataset_drift(base_df = train_dataframe, 
-                                               current_df = test_dataframe)
+                raise Exception(error_message)
 
-            data_validation_artifact = DataValidationArtifact(validation_status = status,
-                                                              valid_train_file_path = self.data_ingestion_artifact.trained_file_path,
-                                                              valid_test_file_path = self.data_ingestion_artifact.test_file_path,
-                                                              invalid_train_file_path = None,
-                                                              invalid_test_file_path = None,
-                                                              drift_report_file_path = self.data_validation_config.drift_report_file_path
+            status = self.detect_dataset_drift(
+                base_df=train_dataframe, current_df=test_dataframe
+            )
 
-
+            data_validation_artifact = DataValidationArtifact(
+                validation_status=status,
+                valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
+                valid_test_file_path=self.data_ingestion_artifact.test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path,
             )
 
             logging.info(f"Data_validation_artifact: {data_validation_artifact}")
 
             return data_validation_artifact
 
-        except Exception as e :
-            raise BankException (e,sys)
-
+        except Exception as e:
+            raise BankException(e, sys)
